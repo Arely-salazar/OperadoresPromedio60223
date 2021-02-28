9da3c79ce60e2f37f24649128d14a0ca6e687be5
Arely <Jairo@LAPTOP-TE9AK6EP>
2021-02-27 18:48:24 -0600

    primer commit

---
+++ src/operadorespromedio60223/OperadoresPromedio60223.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadorespromedio60223;
+
+/**
+ *
+ * @author Jairo
+ */
+public class OperadoresPromedio60223 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre= "JESUS ALFREDO";
+        String matricula="67606";
+        System.out.println(" SOY " + nombre + "Y MI MATRICULA ES" + matricula );}
+}
